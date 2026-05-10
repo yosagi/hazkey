7e8876cb6bc89754618830a8504868b5eee17de5
stakeval0 <stakeval0@gmail.com>
2026-05-11 02:53:01 +0900

    feat: support clause boundary adjustment with Shift+Left/Right
    Allow adjusting the conversion target while the candidate list is open.
    
    - regenerate normal conversion candidates after Shift+Left/Right
    - keep the remaining unconverted text after partial commit
    - allow Esc to return from clause-boundary adjustment to full-range candidates

--- fcitx5-hazkey/src/hazkey_state.cpp
+++ fcitx5-hazkey/src/hazkey_state.cpp
@@ -227,19 +227,29 @@ void HazkeyState::candidateKeyEvent(
     std::vector<std::string> preedit;
     switch (keysym) {
         case FcitxKey_Right:
-            // if (event.key().states() == KeyState::Alt) {
-            candidateList->nextPage();
-            // }
+            if (key.states() == KeyState::Shift) {
+                moveSegmentBoundary(true);
+            } else {
+                candidateList->nextPage();
+            }
             break;
         case FcitxKey_Left:
-            // if (event.key().states() == KeyState::Alt) {
-            candidateList->prevPage();
-            // }
+            if (key.states() == KeyState::Shift) {
+                moveSegmentBoundary(false);
+            } else {
+                candidateList->prevPage();
+            }
             break;
         case FcitxKey_Return:
             candidateCompleteHandler(candidateList);
             break;
         case FcitxKey_Escape:
+            if (isClauseBoundaryAdjusting_) {
+                showNonPredictCandidateList(false);
+                break;
+            }
+            isClauseBoundaryAdjusting_ = false;
+            [[fallthrough]];
         case FcitxKey_BackSpace:
             showPreeditCandidateList();
             break;
@@ -306,7 +316,8 @@ void HazkeyState::candidateCompleteHandler(
     engine_->server().completePrefix(candidateList->globalCursorIndex());
     ic_->commitString(preedit[0]);
     if (preedit.size() > 1) {
-        showNonPredictCandidateList();
+        isClauseBoundaryAdjusting_ = false;
+        showNonPredictCandidateList(false);
     } else {
         reset();
     }
@@ -476,7 +487,11 @@ bool HazkeyState::showCandidateList(bool isSuggest) {
     return response.page_size() > 0;
 }
 
-void HazkeyState::showNonPredictCandidateList() {
+void HazkeyState::showNonPredictCandidateList(bool preserveTarget) {
+    if (!preserveTarget) {
+        engine_->server().moveCursor(1024);
+        isClauseBoundaryAdjusting_ = false;
+    }
     showCandidateList(false);
 
     livePreeditIndex_ = -1;
@@ -532,6 +547,12 @@ void HazkeyState::backCandidateCursor(
     updateCandidateCursor(candidateList);
 }
 
+void HazkeyState::moveSegmentBoundary(bool expand) {
+    isClauseBoundaryAdjusting_ = true;
+    engine_->server().moveCursor(expand ? 1 : -1);
+    showNonPredictCandidateList(true);
+}
+
 /// AUX
 
 void HazkeyState::setCandidateCursorAUX(
@@ -565,6 +586,7 @@ void HazkeyState::reset() {
     isDirectConversionMode_ = false;
     livePreeditIndex_ = -1;
     isCursorMoving_ = false;
+    isClauseBoundaryAdjusting_ = false;
     engine_->server().newComposingText();
     preedit_.clear();
     ic_->inputPanel().reset();
